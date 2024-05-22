1a56b13c6ab88bff7d1e503416a9f211ba728445
Mohtasim Hadi Rafi <okormonno@Mohtasims-MacBook-Air.local>
2024-05-22 16:06:57 -0500

    basic calculator

---
+++ L01/basic_calculator.R
@@ -0,0 +1,9 @@
+(2 + 3) / 5     #Everything after the pound sign is ignored
+(2 + 3) / 5     #Spaces are ignored
+(2 + 3) /       #If your submitted input is not complete then a plus sign appears on the next line. You can then complete the input. If you would rather start over press the Escape key. # nolint
+3 * 2           #3 Times 2
+3^2             #3 to the power 2
+3^.5            #3 to the power 0.5 which is the same as the square root of 3
+sqrt(3)         #square root of 3
+log(10)         #natural log of 10
+exp(log(10))    #exponential of the natural log of 10
