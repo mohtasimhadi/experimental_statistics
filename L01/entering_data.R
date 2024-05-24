350e33d094e99b61ef50591e88489bb5015d8563
Mohtasim Hadi Rafi <mohtasimhadi@gmail.com>
2024-05-24 12:15:56 -0500

    Module Wise Division

--- L01/entering_data.R
+++
@@ -1,5 +0,0 @@
-myData <- read.csv("/Users/okormonno/Workspace/Auburn University/Summer'24/STAT7000 Experimental Statistics I/L01/class.csv")
-myData
-mean(myData$final)
-x <- myData$exam2
-x
---
+++ Module_01_Introduction_to_R/L01/entering_data.R
@@ -0,0 +1,5 @@
+myData <- read.csv("Module_01_Introduction_to_R/L01/class.csv")
+myData
+mean(myData$final)
+x <- myData$exam2
+x
